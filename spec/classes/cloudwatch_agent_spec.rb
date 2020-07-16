f635886ab315dbe86783f8aa11d5059c034f2370
wow73611 <wow73611@gmail.com>
2020-07-16 17:48:57 +0800

    Update spec code

--- spec/classes/cloudwatch_agent_spec.rb
+++ spec/classes/cloudwatch_agent_spec.rb
@@ -8,50 +8,12 @@ describe 'corp104_cloudwatch_agent', :type => :class do
     }
   end
 
-  context 'defaults' do
-    it { is_expected.to contain_corp104_cloudwatch_agent__params }
-    it { is_expected.to contain_package('curl') }
-    it { is_expected.to contain_exec('install-awslogs') }
-    it { is_expected.to contain_concat('awslogs.conf').with(
-      :path => '/var/awslogs/etc/awslogs.conf',
-    )}
-    it { is_expected.to contain_concat__fragment('general').with(
-      :order => '01',
-    ) }
-    it { is_expected.to contain_service('awslogs').with(
-      :ensure => 'running',
-      :enable => true,
-    )}
+  it do
+    should contain_class('corp104_cloudwatch_agent')
   end
 
-  context 'when installing the package' do
-    let :params do
-      {
-        :package_install => true,
-      }
-    end
-
-    it { is_expected.to contain_package('awslogs').with(
-      'ensure' => 'present',
-    )}
-    it { is_expected.to contain_concat('awslogs.conf').with(
-      :path => '/etc/awslogs/awslogs.conf',
-    )}
-
-  end
-
-  context 'when creating log resources' do
-    let :params do
-      {
-        :logs => {
-          '/var/log/syslog' => {
-            'log_stream_name' => 'testing',
-          }
-        }
-      }
-    end
-
-    it { is_expected.to contain_corp104_cloudwatch_agent__log('/var/log/syslog') }
+  it do
+    should compile.with_all_deps
   end
 
 end
